83974eaea8ff8f809c97fb0e19354b328c2294f1
Kostya Tolmachov <tolmak@khtos.com>
2023-04-25 15:09:27 +0100

    proposition 1 a c

--- src/bourbaki.lean
+++ src/bourbaki.lean
@@ -38,19 +38,47 @@ def gen_length (w : W) := nat.find (len_exists S w)
 
 def is_reduced (S : set W) (w : W) [generated_group W S] (l : list S) := (l : list W).prod = w ∧ l.length = gen_length S w
 
+lemma coe_commutes_with_append (l1 l2 : list S) : (coe (l1 ++ l2) : list W) = (coe l1 : list W) ++ (coe l2 : list W) :=
+begin
+   rw coe, rw list_subtype_to_list, apply list.map_append,
+end
+
 lemma len_mul_le (w w' : W) : gen_length S (w * w') ≤ gen_length S w + gen_length S w' :=
 begin
-sorry
-end 
+    have h1 := nat.find_spec (len_exists S w),
+    have h2 := nat.find_spec (len_exists S w'),
+    cases h1 with l1 hl1,
+    cases h2 with l2 hl2,
+    unfold gen_length,
+    rw [←hl1.2, ←hl2.2],
+    have hgp : gen_by_n S (w*w') (l1.length + l2.length),
+         unfold gen_by_n, use l1++l2, split,
+         rw [← hl1.1, ← hl2.1], rw coe_commutes_with_append, rw list.prod_append,
+         rw list.length_append,
+    exact nat.find_le hgp
+end
 
 lemma len_inv (w : W) : gen_length S (w⁻¹) = gen_length S w :=
 begin
 sorry
 end
 
-lemma le_len_rat (w w' : W) : |(gen_length S w : ℤ) - gen_length S w'| ≤ gen_length S (w * w⁻¹) :=
+lemma le_len_rat (w w' : W) : |(gen_length S w : ℤ) - gen_length S w'| ≤ gen_length S (w * w'⁻¹) :=
 begin
-sorry
+    rw abs_le, split,
+    rw neg_le_sub_iff_le_add,
+    rw [←len_inv S (w * w'⁻¹)], rw mul_inv_rev, rw inv_inv, rw add_comm,
+    have h_pr : w' = (w' * w⁻¹) * w, simp,
+    nth_rewrite 0 h_pr, 
+    have hint := len_mul_le S (w' * w⁻¹) w,
+    rw ←int.coe_nat_add,
+    rw int.coe_nat_le_coe_nat_iff, exact hint,
+    rw sub_le_iff_le_add,
+    have h_pr : w = (w * w'⁻¹) * w', simp,
+    nth_rewrite 0 h_pr, 
+    have hint := len_mul_le S (w * w'⁻¹) w',
+    rw ←int.coe_nat_add,
+    rw int.coe_nat_le_coe_nat_iff, exact hint,
 end
 
 lemma prod_reduced (l l' : list S) (w w' : W) ( hgenw : (l : list W).prod = w ) ( hgenw : (l' : list W).prod = w' ) (h_red : is_reduced S (w*w') (l ++ l') ) : is_reduced S w l ∧ is_reduced S w' l':=
